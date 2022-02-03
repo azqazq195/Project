01ba14793afa95dfc551cf7e052e2256909794c9
moseoh <azqazq195@gmail.com>
2022-02-03 21:06:37 +0900

    update

---
+++ Unknown/src/main/kotlin/com/example/unknown/UnknownApplication.kt
@@ -0,0 +1,11 @@
+package com.example.unknown
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UnknownApplication
+
+fun main(args: Array<String>) {
+    runApplication<UnknownApplication>(*args)
+}
